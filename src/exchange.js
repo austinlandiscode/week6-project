921af2164cd9d6ef500f517066346fdb5d8c554a
Austin Landis <austinlandiscode@gmail.com>
2021-02-12 12:46:18 -0800

    update UI logic

--- src/exchange.js
+++ src/exchange.js
@@ -2,7 +2,7 @@ export default class ExchangeRate {
   static convertCurrency() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/8e9efc30f78ccf1b8a7da89c/latest/USD`;
+      const url = `GET https://v6.exchangerate-api.com/v6/8e9efc30f78ccf1b8a7da89c/pair/USD/${convert}/${inputtedNumber}`;
 
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -4,19 +4,17 @@ import './css/styles.css';
 import $ from 'jquery';
 import ExchangeRate from './exchange.js';
 
-let promise = ExchangeRate.convertCurrency();
-promise.then(function(response) {
-
-})
-
+$(document).ready(function() {
   $('#money').click(function() {
     let inputtedNumber = parseInt($('#usd').val());
     $('#usd').val("");
     let convert = $('#currency').val();
-    console.log(inputtedNumber);
+    let promise = ExchangeRate.convertCurrency();
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('#answer').text(`the conversation of ${inputtedNumber}USD and ${convert} is ${body.conversion_result}`)
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
   });
-  
-  promise.then(function(response) {
-    const body = JSON.parse(response);
-    $('#answer').text(`the conversation of ${inputtedNumber}USD and ${convert}`)
-  });
+});
