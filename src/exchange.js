55657424b69219bfbd6b1e9d6e78134e1d276d66
Austin Landis <austinlandiscode@gmail.com>
2021-02-12 15:29:30 -0800

    add high function template literal and update html

--- src/exchange.js
+++ src/exchange.js
@@ -2,7 +2,7 @@ export default class ExchangeRate {
   static convertCurrency(convertedCurrency) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/8e9efc30f78ccf1b8a7da89c/pair/USD/${convertedCurrency}/${inputtedNumber}`;
+      const url = `https://v6.exchangerate-api.com/v6/8e9efc30f78ccf1b8a7da89c/pair/USD/${convertedCurrency}/40`;
 
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import $ from 'jquery';
 import ExchangeRate from './exchange.js';
 
 $(document).ready(function() {
-  $('#money').click(function() {
+  $('#money').submit(function() {
     let convertedCurrency = $('#currency').val();
     let inputtedNumber = parseInt($('#usd').val());
     $('#usd').val("");
@@ -13,6 +13,7 @@ $(document).ready(function() {
     promise.then(function(response) {
       const body = JSON.parse(response);
       $('#answer').text(`the conversation of ${inputtedNumber}USD and ${convertedCurrency} is ${body.conversion_result}`);
+      $('#conversions').append(`<tr><td>USD</td><td>${inputtedNumber}</td><td>${convertedCurrency}</td><td>${body.conversion_result}</td><td>${body.time_last_update_utc}</td></tr>`)
     }, function(error) {
       $('#showErrors').text(`There was an error processing your request: ${error}`);
       console.log(`${inputtedNumber}`);
