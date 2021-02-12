965b586bb37395beddac1a730594de67d64f19ba
Austin Landis <austinlandiscode@gmail.com>
2021-02-12 15:47:54 -0800

    rearrange variable names

--- src/exchange.js
+++ src/exchange.js
@@ -1,8 +1,8 @@
 export default class ExchangeRate {
-  static convertCurrency(convertedCurrency) {
+  static convertCurrency(convertedCurrency, inputtedNumber) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/8e9efc30f78ccf1b8a7da89c/pair/USD/${convertedCurrency}/40`;
+      const url = `https://v6.exchangerate-api.com/v6/8e9efc30f78ccf1b8a7da89c/pair/USD/${convertedCurrency}/${inputtedNumber}`;
 
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -9,14 +9,13 @@ $(document).ready(function() {
     let convertedCurrency = $('#currency').val();
     let inputtedNumber = parseInt($('#usd').val());
     $('#usd').val("");
-    let promise = ExchangeRate.convertCurrency(convertedCurrency);
+    let promise = ExchangeRate.convertCurrency(convertedCurrency, inputtedNumber);
     promise.then(function(response) {
       const body = JSON.parse(response);
       $('#answer').text(`the conversation of ${inputtedNumber}USD and ${convertedCurrency} is ${body.conversion_result}`);
       $('#conversions').append(`<tr><td>USD</td><td>${inputtedNumber}</td><td>${convertedCurrency}</td><td>${body.conversion_result}</td><td>${body.time_last_update_utc}</td></tr>`)
     }, function(error) {
       $('#showErrors').text(`There was an error processing your request: ${error}`);
-      console.log(`${inputtedNumber}`);
     });
   });
 });
