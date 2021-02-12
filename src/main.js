b034b8636b815f0e11e59ffd160611f14e5661ad
Austin Landis <austinlandiscode@gmail.com>
2021-02-12 09:44:45 -0800

    add UI and BI logic, will seperate later.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+
+$('#money').click(function() {
+  const inputtedNumber = parseInt($('#usd').val());
+  $('#usd').val("");
+});
+
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = 
+  }
