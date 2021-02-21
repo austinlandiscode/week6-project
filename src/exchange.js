cf1b395065a6ea0ef996c09b4b2d2226ce6c9889
Austin Landis <austinlandiscode@gmail.com>
2021-02-21 14:41:11 -0800

    error handling

--- src/exchange.js
+++ src/exchange.js
@@ -3,7 +3,6 @@ export default class ExchangeRate {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_Key}/pair/${convertedCurrency}/${convertedCurrencyTwo}/${inputtedNumber}`;
-
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
