e807f970327fee804634a9b4540544e1e82a01bc
Austin Landis <austinlandiscode@gmail.com>
2021-02-25 10:38:45 -0800

    final changes

--- src/main.js
+++ src/main.js
@@ -12,14 +12,12 @@ $(document).ready(function() {
     let convertedCurrencyTwo = $('#currency2').val();
     $('#usd').val("");
     let promise = ExchangeRate.convertCurrency(convertedCurrency, convertedCurrencyTwo, inputtedNumber);
-    console.log(promise);
     promise.then(function(response) {
       const body = JSON.parse(response);
       if (body.result === 'success') {
         $('#answer').text(`${inputtedNumber} ${convertedCurrency} is equal to ${body.conversion_result} ${convertedCurrencyTwo}`);
         $('#conversions').append(`<tr><td>${convertedCurrency}</td><td>${inputtedNumber}</td><td>${convertedCurrencyTwo}</td><td>${body.conversion_rate}</td><td>${body.conversion_result}</td><td>${body.time_last_update_utc}</td></tr>`);
       } else if (body.result === 'error') {
-        console.log(body);
         $('#showErrors').text(`There was an error processing your request ${body["error-type"]}`);
       }
       },function() {
